5659c923b90e40abdb7049c51e284688ca053113
krishnaansh <krishnaansh997@gmail.com>
2021-08-03 17:28:50 +0530

    f

---
+++ odata/static/js/tinmc.js
@@ -0,0 +1,13 @@
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js');
+document.head.appendChild(jQueryScript);
+setTimeout(function(){
+    tinymce.init({
+        selector: 'textarea',
+        height: 500,
+    });
+}, 1000);
+
+setTimeout(function(){
+    $(".tox-notifications-container").hide()
+},1500);
